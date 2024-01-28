4a6fc1832e235415f4337ae8285141a00d6e2133
Victor Tse <victortse70@gmail.com>
2024-01-28 16:16:10 -0500

    Finished about me page requirements

--- Assignment1-ReactPortfolio/src/about.jsx
+++ Assignment1-ReactPortfolio/src/about.jsx
@@ -1,7 +1,34 @@
+import React from "react";
+import MyPhoto from "./images/MyPhoto.jpg";
 export default function About() {
   return (
     <>
-      <p>About</p>
+      <div style={{ textAlign: "center" }}>
+        <h1>About Me</h1>
+        <img
+          src={MyPhoto}
+          alt="MyPhoto"
+          style={{ height: "300px", width: "auto" }}
+        />
+        <p style={{ padding: "0 10em", lineHeight: "1.6", fontSize: "18px" }}>
+          I am an enthusiastic and skilled professional with over 10 years of
+          experience in the semiconductor industry, now diving deeper into
+          Software Engineering Technology and Artificial Intelligence at the
+          University of York. My expertise spans Python, Oracle SQL, and web
+          development, complemented by a strong ability to apply AI and Machine
+          Learning to complex data analysis. Proficient in software architecture
+          and object-oriented programming with C# and Java, I excel in leading
+          projects to innovate and enhance product performance, achieving
+          outstanding results and customer satisfaction.
+        </p>
+        <a
+          href="https://drive.google.com/file/d/1pjslGt71KRCTK4sSQzINt-4TYe8RaVXG/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View my resume
+        </a>
+      </div>
     </>
   );
 }
